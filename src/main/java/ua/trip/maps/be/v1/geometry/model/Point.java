7f3d3b8d075772073f8e54c7241463b01e7ddfc7
Serhii Yermakov <ermakov@dlit.dp.ua>
2021-05-30 18:23:33 +0300

    feat: Convex Hull Service

---
+++ src/main/java/ua/trip/maps/be/v1/geometry/model/Point.java
@@ -0,0 +1,14 @@
+package ua.trip.maps.be.v1.geometry.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Point {
+    private Double x;
+
+    private Double y;
+}
