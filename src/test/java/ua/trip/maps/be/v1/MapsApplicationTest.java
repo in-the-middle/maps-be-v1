6a3887d5e557eaca995895b4d290cda2b6be6858
Serhii Yermakov <ermakov@dlit.dp.ua>
2021-07-25 18:32:34 +0300

    fix: Fix tests

--- src/test/java/ua/trip/maps/be/v1/MapsApplicationTest.java
+++ src/test/java/ua/trip/maps/be/v1/MapsApplicationTest.java
@@ -1,8 +1,13 @@
 package ua.trip.maps.be.v1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ua.trip.maps.be.v1.service.CenterService;
 
 public class MapsApplicationTest {
+    @MockBean
+    CenterService centerService;
+
     @Test
     void contextLoads() {
     }
