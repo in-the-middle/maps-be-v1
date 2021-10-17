aaf8554e4b4db6e17a284a3962fadede5984c99e
Serhii Yermakov <ermakov@dlit.dp.ua>
2021-10-17 21:37:12 +0300

    feat: Auth lib integration

--- src/main/java/ua/trip/maps/be/v1/controller/RouteController.java
+++ src/main/java/ua/trip/maps/be/v1/controller/RouteController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import ua.trip.auth.lib.v1.provider.UserTokenProvider;
 import ua.trip.maps.be.v1.service.RouteService;
 import ua.trip.maps.service.api.RouteApi;
 import ua.trip.maps.service.model.RouteInputDTO;
@@ -14,6 +15,9 @@ import ua.trip.maps.service.model.RouteOutputDTO;
 @RestController
 public class RouteController implements RouteApi {
     private static final Logger LOGGER = LoggerFactory.getLogger(RouteController.class);
+
+    @Autowired
+    UserTokenProvider userTokenProvider;
 
     @Autowired
     RouteService routeService;
