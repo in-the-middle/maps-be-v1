6a22df6e23bb03e73023f1326c203606473fed46
Serhii Yermakov <ermakov@dlit.dp.ua>
2021-11-04 19:57:19 +0200

    fix: Temporary disable auth

--- src/main/java/ua/trip/maps/be/v1/MapsApplication.java
+++ src/main/java/ua/trip/maps/be/v1/MapsApplication.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
-import ua.trip.auth.lib.v1.configuration.AuthConfiguration;
+//import ua.trip.auth.lib.v1.configuration.AuthConfiguration;
 
 @SpringBootApplication
 @EnableAsync
 @EnableFeignClients(basePackages = {"ua.trip.maps.be.v1.valhalla.consumer"})
-@Import(AuthConfiguration.class)
+//@Import(AuthConfiguration.class)
 public class MapsApplication {
     public static void main(String[] args){
         SpringApplication.run(MapsApplication.class, args);
